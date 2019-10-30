df1235538c2d4b51332926148faa1da923fe6785
cisspbru <32341115+cisspbru@users.noreply.github.com>
2019-10-30 14:34:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ f = open(outfile, "a")
 for i in range(1, filelimit + 1): 
 	filename = "page_"+str(i)+".jpg
 	# Recognize the text as string in image using pytesserct 
-	text = str(((pytesseract.image_to_string(Image.open(filename))))) 
+	text = str(((pytesseract.image_to_string(Image.open(filename), lang='rus')))) 
 	text = text.replace('-\n', '')	 
 	# Finally, write the processed text to the file. 
 	f.write(text) 
